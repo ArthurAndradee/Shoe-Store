61be118d4c3f2e006d0362cd1b1d99e4eab866f6
Arthur Andrade <arthurandrad.23@gmail.com>
2024-03-21 11:24:02 -0300

    Feat: Language header finished

---
+++ src/Headers/LanguagePage/header.tsx
@@ -0,0 +1,14 @@
+import './header.css';
+
+function LanguageHeader() {
+  return (
+    <div className="App">
+      <div className='logoBox'>
+        <div>SHOESHOP</div>
+        <img alt='logo' src='https://www.logo.wine/a/logo/Converse_(shoe_company)/Converse_(shoe_company)-Icon-Logo.wine.svg' />
+      </div>
+    </div>
+  );
+}
+
+export default LanguageHeader;
---
+++ src/Headers/LanguagePage/main.tsx
@@ -0,0 +1,17 @@
+import './main.css';
+
+function LanguagePage() {
+  return (
+    <div className="Main">
+        <div className='selectionText'>Select Location & Language</div>
+        <div className='contentBox'>
+            <div className='continent'>AMERICAS</div>
+            <div className='continent'>EUROPE</div>
+            <div className='continent'>ASIA PACIFIC</div>
+            <div className='continent'>AFRICA</div>
+        </div>
+    </div>
+  );
+}
+
+export default LanguagePage;
