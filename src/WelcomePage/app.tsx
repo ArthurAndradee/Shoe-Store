de8288681f96ea4cea7f60d0e79d94ed06e31831
Arthur Andrade <arthurandrad.23@gmail.com>
2024-03-24 15:51:15 -0300

    Feat: Worked in WelcomePage's images

--- src/WelcomePage/app.tsx
+++ src/WelcomePage/app.tsx
@@ -11,11 +11,15 @@ function WelcomePage() {
 
                     <div className='adBox'>
                         <div id="ad-1"></div>
-                        <div id="ad-2"></div>
-                        <div id="ad-3"></div>
+                        <div id="ad-2">
+                            <img  src="https://images-prod.dazeddigital.com/640/azure/dazed-prod/1360/7/1367251.jpg" alt="Shop shoe" />
+                        </div>
+                        <div id="ad-3">
+                            <img src="https://www.ocarafashion.com/wp-content/uploads/2024/03/2197762698.jpg" alt="goth shoes" />
+                        </div>
                     </div>
                     
-                    <div className='adBox'>
+                    <div className='adBox-2'>
                         <div id="ad-4"></div>
                         <div id="ad-5"></div>
                         <div id="ad-6"></div>
