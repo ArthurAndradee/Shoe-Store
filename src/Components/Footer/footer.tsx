733084f7c7e95bca9e97181c307c216976c8af9d
arthur andrade <arthurandrad.23@gmail.com>
2024-03-27 20:56:20 -0300

    Feat: Footer Scratch worked on

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -1,9 +1,44 @@
+import { faTruckFast } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './footer.css';
+import { faFacebookF, faInstagram, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
-    <footer className="App">
-      ABCDEFG
+    <footer>
+      <section className='newslatter-container'>
+        <h4>Não perca nenhuma novidade</h4>
+        <input type='email' />
+        <button type='submit'>-{'>'}</button>
+      </section>
+      <section className='about-container'>
+        <div>
+          <FontAwesomeIcon icon={faTruckFast} />
+        </div>
+        <h4>Frete</h4>
+        <h4>Devoluções e trocas</h4>
+        <h4>Meios de pagamento</h4>
+        <h4>Dúvidas Frequentes</h4>
+        <h4>Cupons e Descontos</h4>
+      </section>
+      <section className='company-container'>
+        <img alt='logo' src='https://www.logo.wine/a/logo/Converse_(shoe_company)/Converse_(shoe_company)-Icon-Logo.wine.svg' />
+        <h4>Nossa História</h4>
+        <p>Conheça mais sobre a história da marca 
+          Converse</p>
+      </section>
+      <section className='socials-container'>
+        <div>
+          <FontAwesomeIcon icon={faFacebookF} />
+          <FontAwesomeIcon icon={faInstagram} />
+          <br />
+          <FontAwesomeIcon icon={faTiktok} />
+          <FontAwesomeIcon icon={faYoutube} />
+        </div>
+        <h4>Redes Sociais</h4>
+        <p>Nos siga nas redes sociais e fique por
+          dentro de todas as novidades</p>
+      </section>
     </footer>
   );
 }
