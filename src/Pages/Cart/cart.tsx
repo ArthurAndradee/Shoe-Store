422df827467c07fbf2b7d67fdd91d4ef949e73c0
arthur andrade <arthurandrad.23@gmail.com>
2024-04-03 12:58:32 -0300

    Feat: HTML sketch for cart page finished

--- src/Pages/Cart/cart.tsx
+++ src/Pages/Cart/cart.tsx
@@ -14,7 +14,27 @@ function Cart() {
                     <h1 id='title'>Carrinho de Compras</h1>
                 </div>
                 <div className='cart-container'>
-                    <div className='items-container'>
+                    <div className='list-container'>
+                        <div className='items-container'>
+                            <div className='item-display'>
+                                <div className='item-img'></div>
+                                <div className='item-info'>
+                                    <div className='item-name'>One Star Academy Pro Preto</div>
+                                    <div className='item-specifications'>
+                                        <div className='item-variation'><b>Cor: </b> BLACK</div>
+                                        <div className='item-variation'><b>Size: </b> 40</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='items-handle'>
+                                <input type="number" id="quantity" name="quantity" min="1" />
+                                <div className='item-price'>R$579,90</div>
+                                <div className='item-functions'>
+                                    <div className='item-wish'>MOVER PARA LISTA DE DESEJOS</div>
+                                    <div className='item-remove'>REMOVER ITEM</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className='cart-overview'>
                         <div className='cart-overview-title'>Resumo do Carrinho</div>
