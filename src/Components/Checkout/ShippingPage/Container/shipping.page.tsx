405278ba43f72cd9c56ee963d266f52ce7fe3e3f
arthur andrade <arthurandrad.23@gmail.com>
2024-04-18 22:34:36 -0300

    Fix: Fixed unused import

--- src/Components/Checkout/ShippingPage/Container/shipping.page.tsx
+++ src/Components/Checkout/ShippingPage/Container/shipping.page.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { set, useForm } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import './shipping.page.css'
 
 function ShippingPage() {
