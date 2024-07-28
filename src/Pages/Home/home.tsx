49be86f59f2fc36c067ef7fd5a29641dc9a3efec
arthur andrade <arthurandrad.23@gmail.com>
2024-07-28 20:35:08 -0300

    Fix: Fixed tag name

--- src/Pages/Home/home.tsx
+++ src/Pages/Home/home.tsx
@@ -10,42 +10,42 @@ function HomePage({ products }: ProductProps) {
         <div className="body">
             <HomeHeader />
             <main>
-                <div className="testingTags-container">
-                    <section className='testingTagBox'>
-                        <div className="background-img background-text" id="testingTag-1">
-                            <h2 className="testingTag-title">Converse X Martine Ali</h2>
-                            <p className="testingTag-description">Explore a fusão única de elementos clássicos e modernos nessa colaboração.</p>
-                            <Link to={'/products/converse-x-martine-ali-chuck-70-xhi-preto'} className="testingTag-buy-link">
-                                <div className="testingTag-buyButton">
+                <div className="sections-container">
+                    <section className='sectionBox'>
+                        <div className="background-img background-text" id="section-1">
+                            <h2 className="section-title">Converse X Martine Ali</h2>
+                            <p className="section-description">Explore a fusão única de elementos clássicos e modernos nessa colaboração.</p>
+                            <Link to={'/products/converse-x-martine-ali-chuck-70-xhi-preto'} className="section-buy-link">
+                                <div className="section-buyButton">
                                     COMPRE AGORA
                                 </div>
                             </Link>
                         </div>
-                        <div className="background-img" id="testingTag-2"></div>
-                        <div className="background-img" id="testingTag-3"></div>
+                        <div className="background-img" id="section-2"></div>
+                        <div className="background-img" id="section-3"></div>
                     </section>
-                    <section className='testingTagBox-2'>
-                        <div className="background-img background-text" id="testingTag-4">
-                            <h2 className="testingTag-title">Modern Lift Chrome Queen</h2>
-                            <p className="testingTag-description">Libere seu ltestingTago punk no estilo assintestingTago Chuck Taylor. </p>
-                            <Link to={'/products/chuck-taylor-all-star-modern-lift-chrome-queen-preto'} className="testingTag-buy-link">
-                                <div className="testingTag-buyButton">
+                    <section className='sectionBox-2'>
+                        <div className="background-img background-text" id="section-4">
+                            <h2 className="section-title">Modern Lift Chrome Queen</h2>
+                            <p className="section-description">Libere seu lsectiono punk no estilo assinsectiono Chuck Taylor. </p>
+                            <Link to={'/products/chuck-taylor-all-star-modern-lift-chrome-queen-preto'} className="section-buy-link">
+                                <div className="section-buyButton">
                                     COMPRE AGORA
                                 </div>
                             </Link>
                         </div>
-                        <div className="background-img" id="testingTag-5"></div>
-                        <div className="background-img" id="testingTag-6"></div>
+                        <div className="background-img" id="section-5"></div>
+                        <div className="background-img" id="section-6"></div>
                     </section>
                 </div>
                 <ProductsRow products={products} />
-                <div className='large-testingTagBox'>
-                    <section className="background-img background-text" id="testingTag-7">
-                        <div className="large-testingTag-box">
+                <div className='large-sectionBox'>
+                    <section className="background-img background-text" id="section-7">
+                        <div className="large-section-box">
                             <h2>ONE STAR</h2>
-                            <p className="testingTag-description">O clássico renovtestingTago: a trtestingTagição do skate dos anos 90, agora com tecnologia de amortecimento CX.</p>
-                            <div className="large-testingTag-buyButton">
-                                <Link to={'/products/one-star-pro-cano-baixo-normal-preto'} className="testingTag-buy-link">COMPRE AGORA</Link>
+                            <p className="section-description">O clássico renovsectiono: a trsectionição do skate dos anos 90, agora com tecnologia de amortecimento CX.</p>
+                            <div className="large-section-buyButton">
+                                <Link to={'/products/one-star-pro-cano-baixo-normal-preto'} className="section-buy-link">COMPRE AGORA</Link>
                             </div>
                         </div>
                     </section>
